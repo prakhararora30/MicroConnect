5e0a16aee89de83e4073f1cad288a8ae4094891a
vinayakmishra13 <vinayakm359@gmail.com>
2026-07-26 21:40:26 +0530

    commit4

---
+++ firebase-config.js
@@ -0,0 +1,68 @@
+/**
+ * Firebase Web Configuration & SDK Initialization
+ * Project: connect-firebase2005
+ */
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { 
+  getAuth, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut,
+  updateProfile
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+import { 
+  getFirestore, 
+  doc, 
+  setDoc, 
+  getDoc, 
+  collection, 
+  addDoc, 
+  query, 
+  orderBy, 
+  limit, 
+  onSnapshot, 
+  updateDoc, 
+  arrayUnion, 
+  arrayRemove, 
+  serverTimestamp 
+} from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+
+// Firebase Configuration Object
+const firebaseConfig = {
+  apiKey: "AIzaSyBMvJ-gu9X_6hSHl0kMyhQg3iJjmDIKJoI",
+  authDomain: "connect-firebase2005.firebaseapp.com",
+  projectId: "connect-firebase2005",
+  storageBucket: "connect-firebase2005.firebasestorage.app",
+  messagingSenderId: "427592900853",
+  appId: "1:427592900853:web:6a3eebee6cf6f3450de5bc"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { 
+  app, 
+  auth, 
+  db, 
+  signInWithEmailAndPassword, 
+  createUserWithEmailAndPassword, 
+  onAuthStateChanged, 
+  signOut, 
+  updateProfile,
+  doc, 
+  setDoc, 
+  getDoc, 
+  collection, 
+  addDoc, 
+  query, 
+  orderBy, 
+  limit, 
+  onSnapshot, 
+  updateDoc, 
+  arrayUnion, 
+  arrayRemove, 
+  serverTimestamp 
+};
